aa669eca37e3090be98eac9ecabccee015868d9a
Matt Early <mearly@ondeckcapital.com>
2013-06-13 14:11:09 -0300

    Update simpleformat.js

--- simpleformat.js
+++ simpleformat.js
@@ -1,8 +1,34 @@
-var odo = odo || {};
-odo.helpers = odo.helpers || {};
+var simpleformat = (function() {
+    
+	var isCharacterKeyPress = function(evt) {
+	    if (typeof evt.which == "undefined") {
+	        // This is IE, which only fires keypress events for printable keys
+	        return true;
+	    } else if (typeof evt.which == "number" && evt.which > 0) {
+	        // In other browsers except old versions of WebKit, evt.which is
+	        // only greater than zero if the keypress is a printable key.
+	        // We need to filter out backspace and ctrl/alt/meta key combinations
+	        return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
+	    }
+	    return false;
+	};
 
-odo.helpers.format = function(value, format, escapeCharacter)
-{
+	var getNewCursorPosition = function(cursor, startVal, endVal, formatLength) {
+		newChar = startVal.charAt(cursor - 1),
+		nextCharInfo = startVal.substring(cursor, formatLength).match(/\d/),
+		nextChar = (nextCharInfo ||[])[0],
+		nextCharPos = (nextCharInfo ||{}).index + cursor;
+		if(newChar.match(/[^\d]/)) {
+			return cursor - 1;
+		}
+		if(nextChar) {
+			return endVal.substring(nextCharPos).indexOf(nextChar) + nextCharPos;
+		}
+		return -1;
+	};
+
+	var format = function(value, format, escapeCharacter)
+	{
 		var numberRegex = new RegExp(escapeCharacter,"g"),
 		numberCount = (format.match(numberRegex)||[]).length,
 		rawInput = value.replace(/[^\d]/g, '').substring(0,numberCount);
@@ -22,71 +48,41 @@ odo.helpers.format = function(value, format, escapeCharacter)
 			}
 		}
 		return newVal;
-}
+	}
 
-odo.helpers.formatInput = function(obj, event, format, escapeCharacter)
-{
-	escapeCharacter = escapeCharacter || 'X';
-	if(isCharacterKeyPress(event)) {
-			var newVal = odo.helpers.format(obj.value, format, escapeCharacter);
-	    	$(obj).change();
+	var formatInput = function(obj, event, format, escapeCharacter) {
+		escapeCharacter = escapeCharacter || 'X';
+		if(isCharacterKeyPress(event)) {
+			var newVal = this.format(obj.value, format, escapeCharacter);
 	    	if (newVal != obj.value) {
-	    		cursor = odo.helpers.getNewCursorPosition(obj.selectionStart, obj.value, newVal, format.length)
+	    		cursor = getNewCursorPosition(obj.selectionStart, obj.value, newVal, format.length)
 	    		obj.value = newVal
 	    		if (cursor != -1) {
 	    			if(obj.setSelectionRange) {
 	    				obj.setSelectionRange(cursor, cursor, 0);
 	    			}
-	    		}
-	    			
-	    	}
-	    }
-}
+	    		}			
+	   		}
+		}
+	};
+    return {format : format, formatInput: formatInput};
+    })();;
 
-odo.helpers.formatPhone = function(obj, event)
+
+simpleformat.formatPhone = function(obj, event)
 {
 		phoneFormat = '(XXX) XXX-XXXX'
-		odo.helpers.formatInput(obj, event, phoneFormat);
+		simpleformat.formatInput(obj, event, phoneFormat);
 }
 
-odo.helpers.formatTaxId = function(obj, event)
+simpleformat.formatTaxId = function(obj, event)
 {
 		var taxIdFormat = 'XX-XXXXXXX';
-		odo.helpers.formatInput(obj, event, taxIdFormat);
+		simpleformat.formatInput(obj, event, taxIdFormat);
 }
 
-odo.helpers.formatSSN = function(obj, event)
+simpleformat.formatSSN = function(obj, event)
 {
 		var taxIdFormat = 'XXX-XX-XXXX';
-		odo.helpers.formatInput(obj, event, taxIdFormat);
-}
-
-odo.helpers.getNewCursorPosition = function(cursor, startVal, endVal, formatLength) {
-	newChar = startVal.charAt(cursor - 1),
-	nextCharInfo = startVal.substring(cursor, formatLength).match(/\d/),
-	nextChar = (nextCharInfo ||[])[0],
-	nextCharPos = (nextCharInfo ||{}).index + cursor;
-	if(newChar.match(/[^\d]/)) {
-		return cursor - 1;
-	}
-	if(nextChar) {
-		return endVal.substring(nextCharPos).indexOf(nextChar) + nextCharPos;
-	}
-	return -1;
-}
-
-// XXX-XX-XXX
-
-
-function isCharacterKeyPress(evt) {
-    if (typeof evt.which == "undefined") {
-        // This is IE, which only fires keypress events for printable keys
-        return true;
-    } else if (typeof evt.which == "number" && evt.which > 0) {
-        // In other browsers except old versions of WebKit, evt.which is
-        // only greater than zero if the keypress is a printable key.
-        // We need to filter out backspace and ctrl/alt/meta key combinations
-        return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
-    }
-    return false;
+		simpleformat.formatInput(obj, event, taxIdFormat);
 }
